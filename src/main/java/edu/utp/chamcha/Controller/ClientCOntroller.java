60c7d4aa57017a66675a55cb57f6eea3b5299b7b
kevinPumaille <pumaillekevin@gmail.com>
2021-05-14 17:36:10 -0500

    nada

--- src/main/java/edu/utp/chamcha/Controller/ClientCOntroller.java
+++ src/main/java/edu/utp/chamcha/Controller/ClientCOntroller.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ClientCOntroller {
+public class ClientController {
     
     @GetMapping("cliente/create")
     public String index(){
