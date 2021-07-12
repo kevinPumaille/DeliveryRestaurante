f800ef8b86631cb1c2f24d19f57a37f27877c316
Jorge <jorge.contreras280898@mail.com>
2021-07-12 18:46:19 -0500

    PAGOS

---
+++ src/main/java/edu/utp/chamcha/model/DetallePedido.java
@@ -0,0 +1,36 @@
+package edu.utp.chamcha.model;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "t_detalle_pedido")
+
+public class DetallePedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id")    
+    private Producto product;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")    
+    private Pedido pedido;
+    private BigDecimal precio;
+    private Integer cantidad; 
+    
+}
---
+++ src/main/java/edu/utp/chamcha/repository/PedidoRespository.java
@@ -0,0 +1,3 @@
+public class PedidoRespository {
+    
+}
